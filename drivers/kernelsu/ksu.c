f3b791168cc5d12b85c65b7434e37ad311bbb90f
MinusPrjkt <sabebsantuii@gmail.com>
2026-05-04 14:39:23 +0000

    Revert "drivers: Update KernelSU-Next to v3.2.2"
    This reverts commit 9530b9e2f55c7fd7dbb657548c049d472df0df6c.

--- drivers/kernelsu/ksu.c
+++ drivers/kernelsu/ksu.c
@@ -1,43 +1,41 @@
-#include "kernel_includes.h"
+#include <linux/export.h>
+#include <linux/fs.h>
+#include <linux/kobject.h>
+#include <linux/init.h>
+#include <linux/module.h>
+#include <generated/utsrelease.h>
+#include <generated/compile.h>
+#include <linux/version.h> /* LINUX_VERSION_CODE, KERNEL_VERSION macros */
 
-// uapi
-#include "include/uapi/app_profile.h"
-#include "include/uapi/feature.h"
-#include "include/uapi/selinux.h"
-#include "include/uapi/supercall.h"
-#include "include/uapi/sulog.h"
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(3, 7, 0)
+#include <uapi/asm-generic/errno.h>
+#else
+#include <asm-generic/errno.h>
+#endif
 
-// includes
-#include "include/klog.h"
-#include "include/arch.h"
-#include "include/ksu.h"
+#define ksu_get_uid_t(x) *(unsigned int *)&(x)
 
-// kernel compat, lite ones
+#include "allowlist.h"
+#include "apk_sign.h"
+#include "app_profile.h"
+#include "arch.h"
+#include "core_hook.h"
+#include "feature.h"
+#include "file_wrapper.h"
 #include "kernel_compat.h"
-
-#include "policy/app_profile.h"
-#include "policy/allowlist.h"
-#include "policy/feature.h"
-#include "manager/apk_sign.h"
-#include "manager/manager_identity.h"
-#include "manager/throne_tracker.h"
-#include "supercall/internal.h"
-#include "supercall/supercall.h"
-#include "infra/su_mount_ns.h"
-#include "infra/file_wrapper.h"
-#include "infra/event_queue.h"
-#include "feature/adb_root.h"
-#include "feature/kernel_umount.h"
-#include "feature/sucompat.h"
-#include "feature/sulog.h"
-#include "runtime/ksud.h"
-#include "sulog/event.h"
-#include "sulog/fd.h"
-
+#include "klog.h"
+#include "ksud.h"
+#include "ksu.h"
+#include "manager.h"
+#include "sucompat.h"
+#include "supercalls.h"
+#include "throne_tracker.h"
+#include "su_mount_ns.h"
 #include "selinux/selinux.h"
 #include "selinux/sepolicy.h"
 
 // selinux includes
+#include <linux/lsm_audit.h>
 #include "avc_ss.h"
 #include "objsec.h"
 #include "ss/services.h"
@@ -49,30 +47,18 @@
 
 // unity build
 #include "tiny_sulog.c"
-#include "policy/allowlist.c"
-#include "policy/app_profile.c"
-#include "policy/feature.c"
-#include "manager/apk_sign.c"
-#include "manager/throne_tracker.c"
-
-#include "supercall/perm.c"
-#include "supercall/dispatch.c"
-#include "supercall/supercall.c"
-
-#include "infra/su_mount_ns.c"
-#include "infra/file_wrapper.c"
-#include "infra/event_queue.c"
-
-#include "feature/adb_root.c"
-#include "feature/kernel_umount.c"
-#include "feature/sucompat.c"
-#include "feature/sulog.c"
-#include "runtime/ksud.c"
-
-#include "sulog/event.c"
-#include "sulog/fd.c"
-
-#include "hook/core_hook.c"	// lsm
+#include "allowlist.c"
+#include "app_profile.c"
+#include "apk_sign.c"
+#include "sucompat.c"
+#include "throne_tracker.c"
+#include "core_hook.c"
+#include "supercalls.c"
+#include "feature.c"
+#include "su_mount_ns.c"
+#include "ksud.c"
+#include "kernel_compat.c"
+#include "file_wrapper.c"
 
 #include "selinux/selinux.c"
 #include "selinux/sepolicy.c"
@@ -80,22 +66,23 @@
 
 #ifdef CONFIG_KSU_TAMPER_SYSCALL_TABLE
 #ifdef CONFIG_ARM64
-#include "hook/syscall_table_hook_arm64.c"
+#include "syscall_table_hook.c"
 #elif CONFIG_ARM
-#include "hook/syscall_table_hook_arm.c"
+#include "syscall_table_hook_arm.c"
 #endif
 #endif
 
-#if defined(CONFIG_KSU_KPROBES_KSUD) && !defined(CONFIG_KSU_TAMPER_SYSCALL_TABLE)
-#include "hook/kp_ksud.c"
+#ifdef CONFIG_KSU_KPROBES_KSUD
+#include "kp_ksud.c"
+#endif
+
+#ifdef CONFIG_KSU_KRETPROBES_SUCOMPAT
+#include "rp_sucompat.c"
 #endif
 
 #ifdef CONFIG_KSU_EXTRAS
 #include "extras.c"
 #endif
-
-// __weak fn's
-#include "kernel_compat.c"
 
 struct cred* ksu_cred;
 
@@ -104,13 +91,14 @@ extern void ksu_supercalls_init();
 // track backports and other quirks here
 // ref: kernel_compat.c, Makefile
 // yes looks nasty
-#if defined(CONFIG_KSU_DEBUG)
-	#define FEAT_1 " +debug"
+#if defined(CONFIG_KSU_KPROBES_KSUD)
+	#define FEAT_1 " +kprobes_ksud"
 #else
 	#define FEAT_1 ""
 #endif
-#if defined(CONFIG_KSU_KPROBES_KSUD) && !defined(CONFIG_KSU_TAMPER_SYSCALL_TABLE)
-	#define FEAT_2 " +kp_ksud"
+
+#if defined(CONFIG_KSU_KRETPROBES_SUCOMPAT)
+	#define FEAT_2 " +rp_sucompat"
 #else
 	#define FEAT_2 ""
 #endif
@@ -120,7 +108,7 @@ extern void ksu_supercalls_init();
 	#define FEAT_3 ""
 #endif
 #if defined(CONFIG_KSU_TAMPER_SYSCALL_TABLE)
-	#define FEAT_4 " +syscall_table_hook"
+	#define FEAT_4 " +sys_call_table_hook"
 #else
 	#define FEAT_4 ""
 #endif
@@ -129,13 +117,8 @@ extern void ksu_supercalls_init();
 #else
 	#define FEAT_5 ""
 #endif
-#if defined(KSU_COMPAT_HAS_EXPORTED_POLICY_RWLOCK)
-	#define FEAT_6 " +policy_rwlock"
-#else
-	#define FEAT_6 ""
-#endif
 
-#define EXTRA_FEATURES FEAT_1 FEAT_2 FEAT_3 FEAT_4 FEAT_5 FEAT_6
+#define EXTRA_FEATURES FEAT_1 FEAT_2 FEAT_3 FEAT_4 FEAT_5
 
 int __init kernelsu_init(void)
 {
@@ -162,16 +145,6 @@ int __init kernelsu_init(void)
 
 	ksu_sucompat_init(); // so the feature is registered
 
-	ksu_kernel_umount_init(); // so the feature is registered
-
-#ifdef CONFIG_KSU_FEATURE_SULOG	
-	ksu_sulog_init(); // so the feature is registered
-#endif
-
-#ifdef CONFIG_KSU_FEATURE_ADBROOT
-	ksu_adb_root_init(); // so the feature is registered
-#endif
-
 	ksu_core_init();
 
 	ksu_allowlist_init();
@@ -186,7 +159,7 @@ int __init kernelsu_init(void)
 	ksu_syscall_table_hook_init();
 #endif
 
-#if defined(CONFIG_KSU_KPROBES_KSUD) && !defined(CONFIG_KSU_TAMPER_SYSCALL_TABLE)
+#ifdef CONFIG_KSU_KPROBES_KSUD
 	kp_ksud_init();
 #endif
 
@@ -197,7 +170,7 @@ int __init kernelsu_init(void)
 	return 0;
 }
 
-device_initcall(kernelsu_init);
+fs_initcall(kernelsu_init);
 
 // MODULE_LICENSE("GPL");
 // MODULE_AUTHOR("weishu");
