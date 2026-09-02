c3d166a4186aaae44be4efdb4c1f19f729735569
ZeSan <zesakain@gmail.com>
2026-09-02 11:37:27 +0000

    tcp: add BBRv2 TCP congestion control

--- net/ipv4/tcp_cong.c
+++ net/ipv4/tcp_cong.c
@@ -175,9 +175,18 @@ void tcp_assign_congestion_control(struct sock *sk)
 
 void tcp_init_congestion_control(struct sock *sk)
 {
-	const struct inet_connection_sock *icsk = inet_csk(sk);
+	struct inet_connection_sock *icsk = inet_csk(sk);
+	const struct tcp_congestion_ops *ca;
 
 	tcp_sk(sk)->prior_ssthresh = 0;
+
+	/* Force BBR2 as default if available */
+	rcu_read_lock();
+	ca = tcp_ca_find("bbr2");
+	if (ca)
+		icsk->icsk_ca_ops = ca;
+	rcu_read_unlock();
+
 	if (icsk->icsk_ca_ops->init)
 		icsk->icsk_ca_ops->init(sk);
 	if (tcp_ca_needs_ecn(sk))
